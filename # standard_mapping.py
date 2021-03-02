235def5ed6c27d882763bf281525d1c295e3ba7b
Ed Guccione <19307105+eguccione@users.noreply.github.com>
2021-03-02 17:23:47 +0000

    created csvs for mapping

--- # standard_mapping.py
+++ # standard_mapping.py
@@ -2,6 +2,8 @@
 # stolen from https://miro.com/app/board/o9J_lW8A4QU=/ by @vickytnz
 # Digital by default Service Standard to Digital Service Standard
 
+import pandas as pd
+
 DbyDSS_to_DSS = {
     'DbyDSS': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
                16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
@@ -11,3 +13,9 @@ DbyDSS_to_DSS = {
 DSS_to_SS = {
     'DSS': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     'SS': [1, 1, 6, 7, 8, 11, 9, 12, 13, 14, 14, 5, 4, 3, 10, 10, 10, 'na']}
+
+DSS_to_SS_df = pd.DataFrame(DSS_to_SS)
+DSS_to_SS_df.to_csv('DSS_to_SS_df.csv')
+
+DbyDSS_to_DSS = pd.DataFrame(DbyDSS_to_DSS)
+DbyDSS_to_DSS.to_csv('DbyDSS_to_DSS.csv')
